86fd76693abcc5d0c6b12ec47aa905c8b8eb6b35
Kenny Li <k3li@ucsd.edu>
2015-02-04 23:54:07 -0800

    initial completion of home, missing linking

--- public/js/GoalPatrolScript.js
+++ public/js/GoalPatrolScript.js
@@ -1,6 +1,24 @@
 //document.getElementById("foot01").innerHTML =
 //"<p>&copy;  " + new Date().getFullYear() + " UCSD COGS120/CSE170. All rights reserved.</p>";
 
+$(document).ready(function() {
+    doFakeAlert();
+    doFakeAcceptReject();
+
+});
+
+function doFakeAlert() {
+    $('.alert').click(function() {
+        alert('Boo!');
+    });
+};
+
+function doFakeAcceptReject() {
+    $('.AccRej').on('click', function() {
+        $(this).parents('div').hide();
+        alert('Done');
+    });
+};
 
 (function () {
     var app = angular.module('myApp', []);
