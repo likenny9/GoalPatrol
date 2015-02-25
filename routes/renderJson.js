5212adfe0f2db051dbd43c8fe8209caa88c3785a
Kenny Li <k3li@ucsd.edu>
2015-02-25 01:26:12 -0800

    Remove unnecessary files and refactored a bit

--- routes/renderJson.js
+++
@@ -1,6 +0,0 @@
-var data = require('../fakedata.json');
-
-exports.html = function(req, res){
-	//Maybe only render the logged in user's details here
-	res.json(data);
-};
--- routes/saveSession.js
+++
@@ -1,6 +0,0 @@
-var data = require('../fakedata.json');
-
-exports.html = function(req, res){
-  req.session.userEmail = req.body.email;
-  res.send();
-};
