1a0b0313dfbd6c7f28ef11b229223d7bd77ef816
Kenny Li <k3li@ucsd.edu>
2015-02-26 23:41:23 -0800

    Fix help to include handlebars and commented out settings/history

--- routes/help.js
+++ routes/help.js
@@ -1,3 +1,16 @@
-exports.html = function(req, res){
-  res.render('help');
-};
+var models = require('../models');
+
+exports.html = function(req, res) {
+	var name = req.session.name;
+
+	if(typeof name == 'undefined') {
+		res.redirect('/login');
+	}
+	else {
+		var users = {
+			"name" : name
+		};
+
+		res.render('help', { "users" : users });
+	}
+}
