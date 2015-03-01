2a035ba5084b851288206e427e4d4326a274751c
Kenny Li <k3li@ucsd.edu>
2015-03-01 00:24:50 -0800

    Include handlebars references in home_alt

--- routes/home2.js
+++ routes/home2.js
@@ -1,3 +1,24 @@
-exports.html = function(req,res){
-	res.render("home_alt");
-}
+var models = require('../models');
+
+exports.html = function(req, res){
+	var sessionUser = req.session.userEmail;
+
+	if(typeof sessionUser == 'undefined') {
+		res.redirect('/login');
+	}
+	else {
+		models.User
+			.find( { "email" : sessionUser })
+			.exec(renderGoals);
+
+		function renderGoals(err, users) {
+			models.Patrol
+				.find( { "user" : users[0]._id })
+				.exec(renderPatrols);
+
+			function renderPatrols(err, patrols) {
+				res.render('home_alt', {'users': users, 'patrols' : patrols });
+			}
+		}
+	}
+};
